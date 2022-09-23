359f4cc9249c0f247ce6cfe450af5592f471e8c7
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2022-09-23 14:13:08 +0200

    stack übung

--- Datastructures/Datastructures/Program.cs
+++ Datastructures/Datastructures/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GenerischeKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var itemStore = new ItemStore<int, double, string>();
+            itemStore.Value1 = 5;
+            itemStore.Value2 = 4.5;
+            itemStore.Value3 = "2";
+
+            var summe = 6 + itemStore.Value1;
+
+        }
+    }
+
+    class ItemStore<TValueType1, TValueType2, TValueType3>
+    {
+        public TValueType1 Value1 { get; set; }
+        public TValueType2 Value2 { get; set; }
+        public TValueType3 Value3 { get; set; }
+    }
+}
