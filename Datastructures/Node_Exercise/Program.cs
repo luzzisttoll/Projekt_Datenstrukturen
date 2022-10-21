02761c87c454a917227fe780d5a0e242a8f90dc8
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2022-10-21 13:59:37 +0200

    added switch nodes + switch value of nodes

--- Datastructures/Node_Exercise/Program.cs
+++ Datastructures/Node_Exercise/Program.cs
@@ -12,11 +12,14 @@ namespace GenerischeKlassen
             linkedList.insertLast(3);
             linkedList.insertLast(4);
             linkedList.insertLast(5);
-            linkedList.insert_After(9, 2);
-            linkedList.insert_First(7);
-            linkedList.DeleteNode(3);
-            linkedList.GetNode(4);
-            //linkedList.printList();
+            //linkedList.InsertAfter(9, 2);
+            //linkedList.insert_First(7);
+            //linkedList.DeleteNode(3);
+            //linkedList.GetNode(4);
+            linkedList.SwitchNode2(new Node(2, null), new Node(4, null));
+            
+            //linkedList.Switchnodes2(new Node(1), new Node(3));
+            linkedList.printList();
             //Console.WriteLine(linkedList.GetNode(4));
         }
     }
